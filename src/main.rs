80e811d75be63e5590bff03ac8f38beba3db23ba
summis <summanentommi@gmail.com>
2020-06-17 20:34:05 +0300

    Now it actually works

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{thread, time};
 use std::net::{TcpStream, TcpListener};
 use std::io::{Read, Write};
+use std::sync::{Arc};
 use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
 use chrono::prelude::*;
@@ -12,7 +13,7 @@ fn rand_string() -> String {
         .collect();
 }
 
-// Server code from https://gist.github.com/mjohnsullivan/e5182707caf0a9dbdf2d
+// Server code based on https://gist.github.com/mjohnsullivan/e5182707caf0a9dbdf2d
 fn handle_read(mut stream: &TcpStream) {
     let mut buf = [0u8 ;4096];
     match stream.read(&mut buf) {
@@ -24,38 +25,43 @@ fn handle_read(mut stream: &TcpStream) {
     }
 }
 
-fn handle_write(mut stream: TcpStream) {
-    let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Hello world</body></html>\r\n";
-    match stream.write(response) {
+fn handle_write(mut stream: TcpStream, response: String) {
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n{}\r\n", response);
+    match stream.write(response.as_bytes()) {
         Ok(_) => println!("Response sent"),
         Err(e) => println!("Failed sending response: {}", e),
     }
 }
 
-fn handle_client(stream: TcpStream) {
+fn handle_client(stream: TcpStream, response: String) {
     handle_read(&stream);
-    handle_write(stream);
+    handle_write(stream, response);
 }
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    let hash = rand_string();
-    println!("Listening for connections on port {}", 8080);
+    let hash = Arc::new(rand_string());
 
     // Start separate thread for loop. `move` allows using hash inside thread.
-    thread::spawn(move || {
-        loop {
-            let now = Utc::now();
-            println!("{} {}", now, hash);
-            thread::sleep(time::Duration::from_secs(5));
-        }
-    });
+    // Cloning allows use of variable in many threads.
+    {
+        let hash = hash.clone().to_string();
+        thread::spawn(move || {
+            loop {
+                let now = Utc::now();
+                println!("{} {}", now, hash);
+                thread::sleep(time::Duration::from_secs(5));
+            }
+        });
+    }
 
+    println!("Listening for connections on port {}", 8080);
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                thread::spawn(|| {
-                    handle_client(stream)
+                let hash = hash.clone().to_string();
+                thread::spawn(move || {
+                    handle_client(stream, hash)
                 });
             }
             Err(e) => {
